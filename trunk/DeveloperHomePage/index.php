081efa2ad7664b7c41a5b2193083bc7f54bcecdc
hirantha <hirantha@a6e525cc-716f-0410-bde9-e0eef6fb3630>
2009-10-27 17:43:09 +0000

    Added remaining folders

---
+++ trunk/DeveloperHomePage/index.php
@@ -0,0 +1,210 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html dir="ltr">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<title>
+	Kmit Developers - home page
+</title>
+<link rel="Stylesheet" href="resources/base_002.css">
+<link rel="Stylesheet" href="resources/layout.css">
+<link rel="Stylesheet" href="resources/core.css">
+<!-- <link id="ctl00_ctl00_Link2" rel="shortcut icon" href="http://www.microsoft.com/windows/Framework/images/favicon.ico"> -->
+<link rel="Stylesheet" href="resources/article.css">
+<link rel="Stylesheet" href="resources/fadingHero.css">
+<link rel="Stylesheet" href="resources/productviewer.css">
+<link rel="Stylesheet" type="text/css" href="resources/TabbedContent.css">
+<link id="ctl00_ctl00_BaseBodyFull_RightColumn_XPTabbedContent_uxCustomStylesheet" rel="Stylesheet" type="text/css" href="">
+<link rel="Stylesheet" href="resources/cps.css">
+<link rel="stylesheet" type="text/css" href="resources/base_003.css">
+<link rel="stylesheet" type="text/css" href="resources/firefox_override_002.css">
+
+<style type="text/css">
+        #full_width_left_column .right_column
+        {
+            height:600px;
+        }
+        
+        #tabContentContainer .sideBar 
+        {	
+			float:right;
+			margin-right:4px;
+        }
+    </style>
+</head><body>
+<div>	
+<div id="ctl00_ctl00_ctl01_AddOnClickTrackingFunctionsToDictionaryPanel"></div>	
+</div>	
+<!--[if lt IE 7]>
+	<link rel="Stylesheet" href="../Framework/styles/base_ieOld.css" />		
+<![endif]-->
+<!--[if IE 7]>
+	<link rel="Stylesheet" href="../Framework/styles/base_ie.css" />
+<![endif]-->
+	<!--[if gte IE 8]>
+	<link rel="Stylesheet" href="../Framework/styles/base_ie8.css" />
+	<![endif]-->
+
+
+<form name="aspnetForm" method="post" action="default.aspx" id="aspnetForm">
+	<div id="top_bkg">
+		<div id="content_bkg">
+			<div id="basePageFrame">
+				<div id="baseHeader">
+					<div id="cps"><div id="ctl00_ctl00_ctl11_uxCPS">	
+	
+	
+	<div style="display: block;" id="lspLink">
+		<div>
+			<a href="http://www.ucsc.cmb.ac.lk/" id="lspLinkText" title="UCSC">University of Colombo School of Computing (UCSC)</a>
+		</div>
+	</div>
+	
+
+</div></div>
+<div class="masthead_header_container cf">
+	<div id="header_left">
+		<div id="header_windows_logo">
+			<a href="http://www.ucsc.cmb.ac.lk/" id="ctl00_ctl00_A1">
+				<img src="resources/ucsc_logo.jpg" width="60" height="41">
+			</a>
+		</div>
+	</div>
+<div id="header_right">
+	<div id="mastheadLinks"><span class="masthead_language"><a href=""></a></span><span class="masthead_country">English</span><span class="masthead_pipe">|</span><a href="#">Other</a></div>	
+	</div>
+</div>					
+					
+<div id="wol_header_nav">	
+
+<link rel="Stylesheet" href="resources/base.css">
+<link rel="Stylesheet" href="resources/firefox_override.css">
+
+<div id="ctl00_ctl00_WindowsNavigation1_NavMenu" class="navMenu">
+	
+<ul class="navMenuList cf">
+  <li>
+    <div class="menuTitle"><a href="#" class="menuTitleLink">Home</a>
+    </div>
+  </li>  
+  <li>
+    <div class="menuTitle"><a href="#" class="menuTitleLink">Product Features</a>
+    </div>
+  </li>
+  <li>
+    <div class="menuTitle"><a href="#" class="menuTitleLink">Product Screen Shots</a>
+    </div>
+  </li>
+  <li>
+    <div class="menuTitle"><a href="#" class="menuTitleLink">Sponsers</a>
+    </div>
+  </li>
+</ul>
+<div class="menuGroupWrapperShadow"></div>
+</div>
+</div>
+</li>
+</ul>
+</div>
+</div>
+</div>
+<div id="body_indented"></div>
+<h1 class="left_indent">Kmit</h1>
+    <div id="full_width_left_column">
+        <div class="left_column">	
+		<div class="featured-downloads">
+		<h2 class="noTopmargin">Important Links</h2>
+		<ul>
+  			<li><p><a href="https://developer.berlios.de/forum/?group_id=11045">Public Forums</a></p></li>
+			<li><p><a href="https://developer.berlios.de/bugs/?group_id=11045">Bug Tracking</a></p></li>
+			<li><p><a href="https://developer.berlios.de/support/?group_id=11045">Tech Support Manager</a></p></li>
+			<li><p><a href="https://developer.berlios.de/feature/?group_id=11045">Feature Requests</a></p></li>
+                  <li><p><a href="https://developer.berlios.de/docman/?group_id=11045">Project Documentation</a></p></li>
+                  <li><p><a href="https://developer.berlios.de/patch/?group_id=11045">Patch Manager</a></p></li>
+                  <li><p><a href="https://developer.berlios.de/mail/?group_id=11045">Mailing Lists</a></p></li>
+                  <li><p><a href="https://developer.berlios.de/pm/?group_id=11045">Tasks</a></p></li>
+                  <li><p><a href="https://developer.berlios.de/survey/?group_id=11045">Surveys</a></p></li>
+                  <li><p><a href="http://svn.berlios.de/svnroot/repos/kmit/trunk">Browse SVN with HTTP</a></p></li>
+            </ul>
+            <p><a href=""></a></p>
+      	</div>
+
+   		<h2 class="bottomMargin">Get the latest Kmit updates</h2>
+    		<ul>
+       		<li><p class=""><a href="https://developer.berlios.de/project/showfiles.php?group_id=11045">Download the latest updates</a></p></li>
+    		</ul>
+   
+   		<!--
+   		<h2 class="bottomMargin">Windows XP Editions</h2>
+    		<ul>
+       		<li><p class=""><a href="http://www.microsoft.com/windowsxp/home/default.mspx">Home Edition</a></p></li><li><p class=""><a href="http://www.microsoft.com/windowsxp/pro/default.mspx">Professional Edition</a></p></li><li><p class=""><a href="http://www.microsoft.com/windowsxp/mediacenter/default.mspx">Media Center Edition</a></p></li><li><p class=""><a href="http://www.microsoft.com/windowsxp/tabletpc/default.mspx">Tablet PC Edition</a></p></li><li><p class=""><a href="http://www.microsoft.com/windowsxp/64bit/default.mspx">Professional x64 Edition</a></p></li>
+    		</ul>
+   		-->
+	</div>
+
+<div class="right_column">
+	<div>
+	    <div id="vistaHero" class="float_left">
+			<a href="#">
+				<img id="ctl00_ctl00_BaseBodyFull_RightColumn_Contentimage7_uxImage" src="resources/laptop_girl_quiz.jpg" height=200 width=400 alt="Windows XP Service Pack 3" style="border-width: 0px;">				
+			</a>	       
+          </div>
+        <div id="vistaCampaignXP" class="float_right">            
+        <div>		
+		<a href="http://developer.berlios.de" title="BerliOS Developer"> <img src="http://developer.berlios.de/bslogo.php?group_id=11045" width="124px" height="32px" border="0" alt="BerliOS Developer Logo"></a>
+	   </div>
+        </div>
+    </div>
+    <div class="right_indent">        
+
+<div id="ctl00_ctl00_BaseBodyFull_RightColumn_XPTabbedContent_TabbedContentControl">	
+	
+	<ul id="ctl00_ctl00_BaseBodyFull_RightColumn_XPTabbedContent_uxTabs" class="tab_xp">
+		<li><a class="firstItem selectedTab" href="http://kmit.freehostia.com">Live Demo</a></li>
+	</ul>
+
+	<div id="tabContentContainer">
+	<div id="ctl00_ctl00_BaseBodyFull_RightColumn_XPTabbedContent_uxTabContentMain" class="mainContent">
+		<h2>Kmit: Knowledge base for MIT</h2><h3>Objectives</h3><br>
+		<!-- <h6>1) Find that file in a few quick clicks</h6> -->
+			<p class="">Design & Develop a Knowledge Base System in web-base for MillenniumIT to share company information with all the stake holders (Customers, Staff, Partners) in order to reduce the cost of customer support, improve staff productivity and eliminate time wasted searching for information across disparate systems such as shared folders and paper documents..</p>
+			<!-- <p class="">Just open the Start menu and begin typing in the Instant Search box—Windows Vista searches file names, application names, the full text of all iles, and metadata, and then displays the results right away.</p> -->
+	</div>	
+	<div id="ctl00_ctl00_BaseBodyFull_RightColumn_XPTabbedContent_uxTabContentSidebar" class="sideBar">
+	 <div class="sidebars"><div class="sidebar">
+          <h3>Team Members</h3>
+           <ul>
+            <li><p class=""><a href="#">MIT/2008/004 - Yapa Ariyasinghe</a></p></li>
+            <li><p class=""><a href="#">MIT/2008/005 - Hirantha Bandara</a></p></li>
+            <li><p class=""><a href="#">MIT/2008/075 - Tharisha Wijeratne</a></p></li>
+            <li><p class=""><a href="#">MIT/2008/076 - Navindu Wijesekera</a></p></li>
+		<li><p class=""><a href="#">MIT/2008/077 - Anju Wijeawardana</a></p></li>
+           </ul>
+       </div>
+      </div>
+	</div>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+
+	
+
+<div id="footer_bkg">
+	<div id="base_page_footer">
+		<div id="footer_internal">
+			<!-- <div id="footer_windows">
+				<img src="resources/win_logo.png" alt="Microsoft Windows" align="bottom">
+			</div> -->
+			<!-- <div id="footer_microsoft">
+				<a href="http://www.microsoft.com/" id="ctl00_ctl00_msComLink"><img src="resources/ms_logo.png" style="margin-top: 8px;" alt="Microsoft" border="0"></a>
+			</div> -->			
+			<div id="footer_links"><p>© 2009 Kmit&nbsp;&nbsp;&nbsp;</p>
+			</div>							
+		</div>
+	</div>
+</div>	
+
+</form>
+</body></html>
